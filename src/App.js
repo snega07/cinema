05a9478d0327fd5ce362c55e9389f126df2cf6eb
snega07 <snegapalanisamy@gmail.com>
2023-06-16 13:37:50 +0530

    route chane

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' exact element={<SplashScreen maxrange={6} />}></Route>
+          <Route path='/cinema' exact element={<SplashScreen maxrange={6} />}></Route>
           <Route path='/login' exact element={<Login />}></Route>
         </Routes>
       </Router>
